c21f2a168cb9be61b411102c0f9bfda55be36e85
Filipa Andrez <filipaandrez@gmail.com>
2023-01-15 22:06:00 +0000

    updates

--- tecnologias/header.php
+++ tecnologias/header.php
@@ -4,10 +4,10 @@
   </div>  
   <nav class="site-navigation">
     <ul class="nav">
-      <li><a href="index.php">Página Inicial</a></li> 
-      <li><a href="tarecos.php">Tarecos</a></li>
-      <li><a href="sobre.php">Sobre Nós</a></li> 
-      <li><a href="doacao.php">Doações</a></li> 
+      <li type="button" title= "Acesso à página principal."><a href="index.php">Página Inicial</a></li> 
+      <li type="button" title= "Conheça os nossos animais!"><a href="tarecos.php">Tarecos</a></li>
+      <li type="button" title= "Informações sobre a associação."><a href="sobre.php">Sobre Nós</a></li> 
+      <li input type="button" title= "Saiba como doar"><a href="doacao.php">Doações</a></li> 
     </ul>
   </nav>
 </header>
