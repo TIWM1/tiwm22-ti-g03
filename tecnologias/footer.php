c7a9be6d28c79c85f3493c5b28cac8a9e9824a2c
joaobraga27 <joaolbraga27@gmail.com>
2023-01-16 19:13:20 +0000

    update

--- tecnologias/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-  <a href="tel:+351229866000" title="Ligue-nos!"><h4>+351 229 866 000</h4></a><p>
-  <a href="https://goo.gl/maps/U1J9WyKtwxC7tcWBA" title = "Saiba onde nos encontramos!"><h4>Av. Carlos de Oliveira Campos, 4475-690 Maia</h4></a>
-  <a href="mailto:tarecosismiau@gmail.com" title = "Envie-nos um email!"><h4>tarecosismiau@gmail.com</h4></a></p>
-  <p><h4>Copyright Tarecos ISMIAU</h4>
-</footer>
-
